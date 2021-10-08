dba81f775defc0a426f8e74a772d7802a03b290e
Reza Niyakan <RezaNiyakan@gmail.com>
2021-10-08 10:49:48 -0500

    completed the js with html lec

---
+++ js/js-with-html-lec.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var jeff= "jeff";
+console.log(jeff);
+
+console.log("Hello! Im in my own file now!");
+
+var returnOfAlert = alert("TGIF")
+console.log(returnOfAlert);
+
+var returnOfConfirm = confirm("Got any plans for this three day weekend?");
+console.log(returnOfConfirm);
+
+var favoriteNuMetalBand = prompt("What's your favorite nu-metal band?");
+console.log("Ha,ha,you like " + favoriteNuMetalBand + "!");
+
+
+
