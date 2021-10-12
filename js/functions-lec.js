0da45fd1eb3834ab4d4214f7dac7320bc631a391
Reza Niyakan <RezaNiyakan@gmail.com>
2021-10-12 15:59:15 -0500

    Finished functions exercise

---
+++ js/functions-lec.js
@@ -0,0 +1,19 @@
+"use strict";
+(function() {
+    //code goes here
+})();
+
+
+// A function is a block of reusable code that performs a specific task.
+// Functions usually take in a input and produca a output.
+
+// Functions we have used so far;
+// - prompt();
+// - alert();
+// - Number();
+// - parseFloat();
+// - parseInt();
+
+function todays
+
+alert("Hello from Codeup");
