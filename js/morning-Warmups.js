a89df5f4aaa7ba7ebd5cec7053c5ef167ca4ec65
Reza Niyakan <RezaNiyakan@gmail.com>
2021-10-15 10:18:56 -0500

    morning-warmups

--- js/morning-Warmups.js
+++ js/morning-Warmups.js
@@ -19,6 +19,21 @@
     console.log(hadBreakfastMessage);
 
 
+    Write a function called 'typeCheck' that takes an input and returns a string of the data type entered. If the data type is a string, return the string "Haha, it's a string of string!"
+        function typeCheck(parameter)   {
+            if (typeof parameter === 'string') {
+                return "Haha, it's a string of a string!";
+            } else  {
+                return typeof parameter;
+            }
+        }
+
+    Tests:
+        console.log(console.log(typeCheck('')));
+    console.log(console.log(typeCheck(isNaN()));
+    console.log(typeCheck(!false * 3));
+    console.log(typeCheck(4 + '4'));
+
 
 })();
 
